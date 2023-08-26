0afbe2dc544d2bee35c3a3e5bf72f9e6a34ac828
cartrugs <cartrugs@gmail.com>
2023-08-26 10:07:33 +0200

    Auth controllers, auth routes, validation jwt: FUNCIONANDO (login: En PROCESO)

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
  * Importación de librerías.
  */
 const express = require('express'); // Librería para crear y manejar servidores web en Node.js.
-
+const cors = require ('cors');
+const cookieParser = require('cookie-parser');
 
 /**
  * Importación del módulo de conexión a la base de datos (mongoose en app.js) {desetructuración}
@@ -30,8 +31,16 @@ dbConnect(); // Función para establecer la conexión a la base de datos MongoDB
  * Configuración de middlewares
  */
 
+//CORS
+app.use(cors());
+
+//COOKIEPARSER
+app.use(cookieParser());
+
+//parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false })); // Middleware para analizar los datos de formularios en las peticiones.
 
+//parse application/json
 app.use(express.json()); // Middleware para analizar el cuerpo de las peticiones en formato JSON.
 
 /*--------------------------------------- RUTAS ---------------------------------------*/
@@ -41,7 +50,7 @@ app.use(express.json()); // Middleware para analizar el cuerpo de las peticiones
  */
 app.use('/api/v1/blog', require('./routes/backRoutes')); // Utiliza las rutas definidas en el archivo backRoutes.js para las peticiones bajo el prefijo '/api/v1/blog'
 
-app.use('/api/v1/blog', require('./routes/authRouters')); // Utiliza las rutas definidas en el archivo authRoutes.js para las peticiones bajo el prefijo '/api/v1/blog'
+app.use('/api/v1/auth', require('./routes/authRouters')); // Utiliza las rutas definidas en el archivo authRoutes.js para las peticiones bajo el prefijo '/api/v1/blog'
 
 /**
  * Inicia el servidor
